1c7d6af3ca49cccf79192cb4a597572d4b916715
Motaz Shrideh <dev@mws.per.jo>
2025-05-28 14:21:22 +0300

    "انشاء شاشة تحميل"

--- LawLib.py
+++ LawLib.py
@@ -1,5 +1,5 @@
 # pyinstaller --noconfirm --onefile --windowed LawLib.py --icon=ico.ico --splash=splash.jpg
-# gh release create v1.0.7 output/LawLibInstaller.exe --title "الإصدار 1.0.7" --notes "تنسيق النتائج بشكل افضل"
+# gh release create v1.0.7 output/LawLibInstaller.exe --title "الإصدار 1.0.7" --notes "انشاء شاشة تحميل"
 import base64
 import json
 import logging
